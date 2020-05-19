e9858392ecbe9766434e2d3ecd6f5729d13043b2
lee <rbsghks01@gmail.com>
2020-05-19 15:55:46 +0900

    third commit

--- Assets/Ch_Main.cs
+++ Assets/Ch_Main.cs
@@ -11,16 +11,19 @@ public class NativeAPI {
     public static extern void showHostMainWindow();
 }
 #endif
-public class Ch_01 : MonoBehaviour
+public class Ch_Main : MonoBehaviour
 {
   private Transform tr;
   public float moveSpeed = 30.0f;
   public GameObject Target;
+  GameObject[] gameobj;
+
   // Start is called before the first frame update
   // Update is called once per frame
   void Start()
   {
     tr = GetComponent<Transform>();
+    gameobj = GameObject.FindGameObjectsWithTag("Character");
   }
   void Update()
   {
@@ -30,8 +33,30 @@ public class Ch_01 : MonoBehaviour
   }
   void hideObject(string charName)
   {
-    GameObject gameobj=GameObject.Find(charName);
-    gameobj.SetActive(false);
+    Debug.Log(gameobj.Length);
+    foreach (GameObject ex in gameobj)
+    {
+      Debug.Log(ex.name);
+
+      if (charName == ex.name)
+      {
+        ex.SetActive(false);
+      }
+    }
+
+  }
+  void onObject(string charName)
+  {
+    foreach (GameObject ex in gameobj)
+    {
+      Debug.Log(ex.name);
+
+      if (charName == ex.name)
+      {
+        ex.SetActive(true);
+      }
+    }
+
   }
   void Walking()
   {
@@ -59,8 +84,9 @@ public class Ch_01 : MonoBehaviour
   {
     GUIStyle style = new GUIStyle("button");
     style.fontSize = 30;
-    Debug.Log("싸발적이고");
+
     if (GUI.Button(new Rect(3, -3, 200, 200), "걷자", style)) Walking();
-    if (GUI.Button(new Rect(10, 10, 200, 200), "싸발적이고 2", style)) hideObject("Ch_01");
+    if (GUI.Button(new Rect(500, 0, 200, 200), "캐릭터 숨김", style)) hideObject("Ch_01");
+    if (GUI.Button(new Rect(300, 0, 200, 200), "캐릭터 표시", style)) onObject("Ch_01");
   }
 }
